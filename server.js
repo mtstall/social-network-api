f16ae1a2d9b42710246101b25270460b79928afa
mtstall <mtstallings95@gmail.com>
2023-04-08 11:14:28 -0400

    deleting routes from server.js

--- server.js
+++ server.js
@@ -12,19 +12,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
-// Update user
-app.put("/api/users/:id", async (req, res) => {
-  User.findOneAndUpdate({ _id: req.params.id }, req.body, (err, result) => {
-    if (result) {
-      res.status(200).json(result);
-      console.log(`Updated: ${result}`);
-    } else {
-      console.log('Uh Oh, something went wrong');
-      res.status(500).json({ message: 'something went wrong' });
-    }
-  });
-});
-
 // Add a new friend
 app.post("/api/users/:userId/friends/:friendId", (req, res) => {
   User.findOneAndUpdate({ _id: req.params.userId }, { $addToSet: { friends: req.params.friendId  } }, (err, result) => {
@@ -51,76 +38,6 @@ app.delete("/api/users/:userId/friends/:friendId", (req, res) => {
   });
 });
 
-// THOUGHT ROUTES
-// Creates a new thought
-app.post("/api/thoughts", (req, res) => {
-  const newThought = new Thought({
-    thoughtText: req.body.thoughtText,
-    username: req.body.username,
-  });
-  newThought.save();
-  if (newThought) {
-    res.status(201).json(newThought);
-  } else {
-    console.log("Uh Oh, something went wrong");
-    res.status(500).json({ error: "Something went wrong" });
-  }
-});
-
-// Finds all thoughts
-app.get("/api/thoughts", (req, res) => {
-  // Using model in route to find all documents that are instances of that model
-  Thought.find({}, (err, result) => {
-    if (result) {
-      res.status(200).json(result);
-    } else {
-      console.log("Uh Oh, something went wrong");
-      res.status(500).json({ error: "Something went wrong" });
-    }
-  });
-});
-
-// Get a thought by its id
-app.get("/api/thoughts/:id", (req, res) => {
-  // Using model in route to find all documents that are instances of that model
-  Thought.find({_id: req.params.id }, (err, result) => {
-    if (result) {
-      res.status(200).json(result);
-    } else {
-      console.log("Uh Oh, something went wrong");
-      res.status(500).json({ error: "Something went wrong" });
-    }
-  });
-});
-
-// Finds first document matching thought parameter and deletes
-app.delete("/api/thoughts/:id", (req, res) => {
-  Thought.findOneAndDelete(
-    { id: req.params.id },
-    (err, result) => {
-      if (result) {
-        res.status(200).json(result);
-        console.log(`Deleted: ${result}`);
-      } else {
-        console.log("Uh Oh, something went wrong");
-        res.status(500).json({ error: "Something went wrong" });
-      }
-    }
-  );
-});
-
-// Update thought
-app.put("/api/thoughts/:id", async (req, res) => {
-  Thought.findOneAndUpdate({ id: req.params.id }, req.body, (err, result) => {
-    if (result) {
-      res.status(200).json(result);
-      console.log(`Updated: ${result}`);
-    } else {
-      console.log('Uh Oh, something went wrong');
-      res.status(500).json({ message: 'something went wrong' });
-    }
-  });
-});
 
 db.once("open", () => {
   app.listen(PORT, () => {
